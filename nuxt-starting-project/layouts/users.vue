0102b74adb6911d4778662382e571dc60c2f7fd0
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-14 09:38:34 +0300

    working with components in nuxt

--- nuxt-starting-project/layouts/users.vue
+++ nuxt-starting-project/layouts/users.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
+    <TheHeader/>
     <header>
         USERS SECTION
     </header>
     <nuxt/>
   </div>
 </template>
+<script>
+import TheHeader from '@/components/TheHeader'
 
+export default {
+  components:{
+    TheHeader
+  }
+}
+</script>
 <style>
 </style>
