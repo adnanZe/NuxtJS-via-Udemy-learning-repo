bf9fbc4706700498f59a67a66d7f003377734eae
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-17 11:00:10 +0300

    using Promise based asyncData

--- prj-page-views-01-first-pages-fonts/pages/posts/index.vue
+++ prj-page-views-01-first-pages-fonts/pages/posts/index.vue
@@ -10,16 +10,20 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
+  asyncData(context) {
+    return new Promise((resolve, reject) => setTimeout(() => {
+      resolve({
         loadedPosts: [
           { id: "1", isAdmin: true, thumbnail: "https://picsum.photos/1000", title: 'Hello there!', previewText: "This is my first post!" },
           { id: "2", isAdmin: true, thumbnail: "https://picsum.photos/1200", title: 'Hello there! - second time!', previewText: "This is my second post!" },
           { id: "3", isAdmin: true, thumbnail: "https://picsum.photos/1300", title: 'Hello there! - third time!', previewText: "This is my third post!" }
         ]
       })
-    }, 3000)
+    }, 3000))
+      .then(data => data)
+      .catch(e => {
+        context.error(new Error(e))
+      })
   }
 }
 </script>
