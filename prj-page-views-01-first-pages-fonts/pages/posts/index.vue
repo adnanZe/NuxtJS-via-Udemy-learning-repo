c12ce71f9afe80831b64b22c7a5cdeaf2ba313f3
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-17 10:15:46 +0300

    assignment with asyncData

--- prj-page-views-01-first-pages-fonts/pages/posts/index.vue
+++ prj-page-views-01-first-pages-fonts/pages/posts/index.vue
@@ -9,6 +9,17 @@ import PostList from '@/components/Posts/PostList';
 export default {
   components: {
     PostList,
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          { id: "1", isAdmin: true, thumbnail: "https://picsum.photos/1000", title: 'Hello there!', previewText: "This is my first post!" },
+          { id: "2", isAdmin: true, thumbnail: "https://picsum.photos/1200", title: 'Hello there! - second time!', previewText: "This is my second post!" },
+          { id: "3", isAdmin: true, thumbnail: "https://picsum.photos/1300", title: 'Hello there! - third time!', previewText: "This is my third post!" }
+        ]
+      })
+    }, 3000)
   }
 }
 </script>
