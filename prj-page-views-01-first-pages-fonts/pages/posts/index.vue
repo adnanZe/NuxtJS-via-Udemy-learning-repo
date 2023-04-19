ca699f202440af200c355bc74d9a393a7ed328fa
AdnanZe <adnan.zecheria@yahoo.com>
2023-04-19 08:30:30 +0300

    working with fetch

--- prj-page-views-01-first-pages-fonts/pages/posts/index.vue
+++ prj-page-views-01-first-pages-fonts/pages/posts/index.vue
@@ -12,13 +12,7 @@ export default {
   },
   fetch(context) {
     return new Promise((resolve, reject) => setTimeout(() => {
-      resolve({
-        loadedPosts: [
-          { id: "1", isAdmin: true, thumbnail: "https://picsum.photos/1000", title: 'Hello there!', previewText: "This is my first post!" },
-          { id: "2", isAdmin: true, thumbnail: "https://picsum.photos/1200", title: 'Hello there! - second time!', previewText: "This is my second post!" },
-          { id: "3", isAdmin: true, thumbnail: "https://picsum.photos/1300", title: 'Hello there! - third time!', previewText: "This is my third post!" }
-        ]
-      })
+      resolve()
     }, 3000))
       .then(data => context.store.commit('setPosts', data.loadedPosts))
       .catch(e => {
--- prj-page-views-01-first-pages-fonts/store/index.js
+++ prj-page-views-01-first-pages-fonts/store/index.js
@@ -11,7 +11,15 @@ const createStore = () => new Vuex.Store({
     },
     actions: {
         nuxtServerInit(vuexContext, context){
-            
+            return new Promise((resolve, reject) => setTimeout(() => {
+           vuexContext.commit('setPosts', [
+              { id: "1", isAdmin: true, thumbnail: "https://picsum.photos/1000", title: 'Hello there!', previewText: "This is my first post!" },
+              { id: "2", isAdmin: true, thumbnail: "https://picsum.photos/1200", title: 'Hello there! - second time!', previewText: "This is my second post!" },
+              { id: "3", isAdmin: true, thumbnail: "https://picsum.photos/1300", title: 'Hello there! - third time!', previewText: "This is my third post!" }
+            ]
+          )
+            resolve();
+            }, 3000))
         },
         setPosts(vuexContext, posts){
             vuexContext.commit('setPosts', posts)
